4d243140d495049797688e0eff78f637e53dfa87
UfukAkyuzYildirim <yildirimufukakyuz@gmail.com>
2024-11-22 00:08:20 +0300

    Api gateway project added. Completed necessary configurations for lightning microservice routing.

---
+++ Gateway.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Gateway.Application
+{
+    public class Class1
+    {
+
+    }
+}
--- LightingService.API/Controllers/TestController.cs
+++ LightingService.API/Controllers/TestController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace LightingService.API.Controllers
 {
     [ApiController]
+    //[Route("api/[controller]/[action]")] //to add action name in route
     [Route("api/[controller]")]
     public class TestController:ControllerBase
     {
@@ -18,6 +19,14 @@ namespace LightingService.API.Controllers
         {
             var response = await _mediator.Send(new TestRequest { Id = id });
             return Ok(response);
+        }
+
+        [HttpGet("GetTestData2")]
+        public async Task<IActionResult> GetTestData2()
+        {
+            // Perform necessary logic here
+
+            return BadRequest();
         }
     }
 }
